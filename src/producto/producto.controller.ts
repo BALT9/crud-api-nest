aeaeadaeac56018f65e9a1211d9cf1f6b7c3f084
willy <willybalt.2017@gmail.com>
2025-07-03 11:11:56 -0400

    se agrego el servicio producto

--- src/producto/producto.controller.ts
+++ src/producto/producto.controller.ts
@@ -1,4 +1,33 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Delete, Get, Patch, Post } from '@nestjs/common'; // importar get,post,delete,update
 
+// ruta base 'producto'
 @Controller('producto')
-export class ProductoController {}
+export class ProductoController {
+    // aqui van las funciones
+
+    // funcion que retorna el tipo de dato
+    @Get()
+    funListar():string{
+        return "Listar Producto";
+    }
+
+    @Post()
+    funCrear():string{
+        return "Guardar producto..";
+    }
+
+    @Get(":id")
+    funMostrar(){
+        return "Mostrando producto";
+    }
+
+    @Patch(":id")
+    funModificar(){
+        return "Modificar producto";
+    }
+
+    @Delete(":id")
+    funEliminar(){
+        return "Eliminar producto";
+    }
+}
--- src/producto/producto.module.ts
+++ src/producto/producto.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ProductoController } from './producto.controller';
+import { ProductoService } from './producto.service';
 
 @Module({
-//   importa el controlador   
-  controllers: [ProductoController]
+    //   importa el controlador   
+    controllers: [ProductoController],
+    providers: [ProductoService]
 })
-export class ProductoModule {}
+export class ProductoModule { }
