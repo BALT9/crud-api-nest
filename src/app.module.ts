aa83f671813e6cf033f2facd39e366ceedeb35f1
willy <willybalt.2017@gmail.com>
2025-07-05 21:11:50 -0400

    crud para categoria

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductoModule } from './producto/producto.module';
 import { UsuarioModule } from './usuario/usuario.module';
+import { CategoriaModule } from './categoria/categoria.module';
 
 @Module({
   // importa los modulos creados 
-  imports: [ProductoModule, UsuarioModule],
+  imports: [ProductoModule, UsuarioModule, CategoriaModule],
   controllers: [AppController],
   providers: [AppService],
 })
