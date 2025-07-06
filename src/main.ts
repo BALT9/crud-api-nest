a80d1c93273e8525d45b02d84ee1227c5b90a3b8
willy <willybalt.2017@gmail.com>
2025-07-06 11:55:19 -0400

    se agrego swager

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  //habilitamos swager
+  const config = new DocumentBuilder()
+    .setTitle('CRUD Productos')
+    .setDescription('Api de productos')
+    .setVersion('1.0')
+    .addTag('producto')
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory); 
+  
   //habilta la habilitacion de la validacion
   app.useGlobalPipes(new ValidationPipe({
     // disableErrorMessages: true
